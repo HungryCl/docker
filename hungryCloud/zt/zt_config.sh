7625b1b2b699d2af11956180bad094a8d89f03e6
Tejas Menon <tejas.menon@hotmail.com>
2021-01-02 16:47:49 -0800

    add trusted domains

--- hungryCloud/zt/zt_config.sh
+++ hungryCloud/zt/zt_config.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/bash
 
 { 
-  sleep 5
+  sleep 3
+
   zerotier-cli join 12ac4a1e7166f279
+
+  sleep 3
+
+  read ZTIP <<< $(zerotier-cli listnetworks | \
+      awk -F '[\/ ]+' '$9 !~ /<ZT/ { print $9 }')
+  
+  docker exec hungrycloud_app_1 sudo -u www-data php occ \
+         config:system:set \
+         trusted_domains 2 --value=$ZTIP
 } &
+
 exec zerotier-one
